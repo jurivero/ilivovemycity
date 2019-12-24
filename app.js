bc696e51fcd7bcbf30b94bfdca1a2f938ecde633
jurivero <jurivero@hotmail.com>
2019-12-23 21:40:36 -0500

    make it easier

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname +'/public'));
 app.get('/',routes.home);
 app.get('/:city',routes.city);
 
-var port= process.env.port || 8080;
+var port= process.env.port || 80;
 var server=app.listen(port, function(req,res){
 console.log("Catch the action at http://localhost:"+port);
 })
